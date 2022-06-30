b4cced1670ca18434d52b2cc970322e15763c49a
Nef <maximenef1@gmail.com>
2022-06-30 20:13:35 +0200

    Revert "remove pl"
    This reverts commit 48f01a7148abd9f19335d6021a5713b9a21da7a9.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
-const optimizedImages = require("next-optimized-images");
-const withPlugins = require("next-compose-plugins");
+
 module.exports = {
   reactStrictMode: false,
   webpack(config) {
@@ -11,21 +10,7 @@ module.exports = {
 
     return config;
   },
-
   images: {
-    loader: "akamai",
     domains: ["images.prismic.io"],
   },
 };
-
-module.exports = withPlugins([
-  [
-    optimizedImages,
-    {
-      /* config for next-optimized-images */
-      target: "serverless",
-    },
-  ],
-
-  // your other plugins here
-]);
