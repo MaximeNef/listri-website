7eca2c99e8e832b45b149c57247539621887a290
Nef <maximenef1@gmail.com>
2022-07-06 15:10:13 +0200

    process card

--- src/components/hero/processHero/processContent/index.jsx
+++ src/components/hero/processHero/processContent/index.jsx
@@ -137,6 +137,7 @@ const ProcessContent = () => {
             <Container className='min-h-[8px] min-w-[16px] rounded-full  bg-gradient-to-b from-[#FF7E00] to-[#FF7E00]/50  '></Container>
           </Flex>
         </Container>
+        <Container className=' w-28  md:hidden'></Container>
       </Flex>
     </Container>
   );
