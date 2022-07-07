b5af7b28193945931325393e8ac66e35a6595195
Nef <maximenef1@gmail.com>
2022-07-07 16:37:32 +0200

    test api key send grid

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const optimizedImages = require("next-optimized-images");
 const withPlugins = require("next-compose-plugins");
 module.exports = {
   reactStrictMode: false,
+  env: {
+    SENDGRID_API_KEY:
+      "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",
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
