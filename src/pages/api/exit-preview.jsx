43641c5b97b57e67c7b51d665edf2eb05f83b074
Nef <maximenef1@gmail.com>
2022-07-15 17:46:05 +0200

    add nextjs file

--- src/pages/api/exit-preview.jsx
+++ src/pages/api/exit-preview.jsx
@@ -1,4 +1,5 @@
-import * as prismicNext from "@prismicio/next";
-export default async function handler(req, res) {
-  prismicNext.exitPreview({ res, req });
+import { exitPreview } from "@prismicio/next";
+
+export default async function exit(req, res) {
+  await exitPreview({ res, req });
 }
--- src/pages/api/mail.jsx
+++ src/pages/api/mail.jsx
@@ -1,5 +1,5 @@
-export default (req, res) => {
-  const main = JSON.parse(JSON.stringify(req.main));
-  console.log(main, "main");
-  res.status(200).json({ status: "ok" });
-};
+// export default (req, res) => {
+//   const main = JSON.parse(JSON.stringify(req.main));
+//   console.log(main, "main");
+//   res.status(200).json({ status: "ok" });
+// };
