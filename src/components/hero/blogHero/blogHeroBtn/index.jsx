ecf1902087a372be2fed405fab961e00c09e5dd0
Nef <maximenef1@gmail.com>
2022-07-15 18:10:36 +0200

    fix btn blog

--- src/components/hero/blogHero/blogHeroBtn/index.jsx
+++ src/components/hero/blogHero/blogHeroBtn/index.jsx
@@ -3,7 +3,7 @@ import Container from "../../../shared/composers/container";
 
 const BlogHeroBtn = () => {
   return (
-    <Container className=' z-20  px-[24px] py-[12px] text-center rounded-[30px]  bg-gradient-to-r from-[#FF7E00] to-[#F9CCA6]  text-white font-medium  shadow-realisationCard'>
+    <Container className=' z-10  px-[24px] py-[12px] text-center rounded-[30px]  bg-gradient-to-r from-[#FF7E00] to-[#F9CCA6]  text-white font-medium  shadow-realisationCard'>
       <Link href='/blogs' passHref>
         <a>{" Découvrez nos articles"}</a>
       </Link>
