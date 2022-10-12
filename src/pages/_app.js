2d50b1eccf0f8aa7448cc281b41fe3c16d6ba40b
Nef <maximenef1@gmail.com>
2022-10-12 10:53:19 +0200

    add google analytique

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,7 @@ import { PrismicPreview } from "@prismicio/next";
 import { linkResolver } from "../../prismicio";
 import { repositoryName } from "../../prismicio";
 import { useEffect, useState } from "react";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   console.log("render");
@@ -21,17 +22,33 @@ export default function App({ Component, pageProps }) {
   }, []);
 
   return (
-    <PrismicProvider
-      linkResolver={linkResolver}
-      internalLinkComponent={({ href, children, ...props }) => (
-        <Link href={href} passHref>
-          <a {...props}>{children}</a>
-        </Link>
-      )}
-    >
-      <PrismicPreview repositoryName={repositoryName}>
-        <Component {...pageProps} />
-      </PrismicPreview>
-    </PrismicProvider>
+    <>
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy='lazyOnload' id='my-script'>
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
+      <PrismicProvider
+        linkResolver={linkResolver}
+        internalLinkComponent={({ href, children, ...props }) => (
+          <Link href={href} passHref>
+            <a {...props}>{children}</a>
+          </Link>
+        )}
+      >
+        <PrismicPreview repositoryName={repositoryName}>
+          <Component {...pageProps} />
+        </PrismicPreview>
+      </PrismicProvider>{" "}
+    </>
   );
 }
