e51cd4127dfe481c429449efb42de40d85ff48f7
Nef <maximenef1@gmail.com>
2022-11-28 18:13:16 +0100

    remove user track folder

--- public/userTrack/server/helpers/getUTCMinutesOffset.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once '../login.php';
-
-    $query = "SELECT TIMESTAMPDIFF(MINUTE, UTC_TIMESTAMP, NOW())";
-    $stmt = $db->prepare($query);
-    $stmt->execute();
-
-    echo $stmt->fetchColumn();
-?>
