a57da81a953ed7d8743ed070ad987ddf4228cbf7
Nef <maximenef1@gmail.com>
2022-12-30 14:36:05 +0100

    Revert "Merge branch 'services_referencement' into dev"
    This reverts commit 6caf11195596c93bdd0765a8c2b2875bc93763b2, reversing
    changes made to 8926030dbd2f1e157dff2794c3ac8e2422cd691c.

--- src/components/services/servicesDetail/TitleServiceDetail/index.jsx
+++ src/components/services/servicesDetail/TitleServiceDetail/index.jsx
@@ -2,7 +2,7 @@ import Container from "../../../shared/composers/container";
 
 const TitleServiceDetail = ({ title }) => {
   return (
-    <Container className='text-[48px]  font-bold text-center leading-[50px] tracking-[0.23px] text-transparent bg-clip-text bg-gradient-to-r via-orangeLight from-rougeLight to-saumon '>
+    <Container className='text-[52px] font-bold text-center leading-[50px] tracking-[0.23px] text-transparent bg-clip-text bg-gradient-to-r via-orangeLight from-rougeLight to-saumon w-fit'>
       {title}
     </Container>
   );
