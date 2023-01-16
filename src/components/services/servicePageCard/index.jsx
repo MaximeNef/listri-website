78200f198c0df26a9fcfd07cc06e47d759885b0b
Nef <maximenef1@gmail.com>
2023-01-16 13:10:49 +0100

    save mode

--- src/components/services/servicePageCard/index.jsx
+++ src/components/services/servicePageCard/index.jsx
@@ -14,12 +14,12 @@ const ServicePageCard = ({
   CTA,
   ancre,
 }) => {
-  const svg = `<svg width="390" height="98" viewBox="0 0 390 98" fill="none" xmlns="http://www.w3.org/2000/svg">
+  const svg = `<svg width="100%" height="100%"  viewBox="0 0 390 98" fill="none" xmlns="http://www.w3.org/2000/svg">
   <path d="M0 0C0 24 18 48 48.5405 49H341C368.062 49 390 70.938 390 98V98H0V0Z" fill="${
     blueBg ? "#283445" : "#F5F5F7"
   }"/>`;
 
-  const svgBottom = `<svg width="390" height="98" viewBox="0 0 390 98" fill="none" xmlns="http://www.w3.org/2000/svg">
+  const svgBottom = `<svg width="100%" height="100%"  viewBox="0 0 390 98" fill="none" xmlns="http://www.w3.org/2000/svg">
   <path d="M0 98C0 74 18 50 48.5405 49H341C368.062 49 390 27.062 390 0H0V98Z" fill="${
     blueBg ? "#283445" : "#F5F5F7"
   }"/>
