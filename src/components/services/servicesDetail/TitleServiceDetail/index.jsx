d557bf39d59d4e3447da7cadf8704ecec6f6778f
Nef Maxime <maximenef1@gmail.com>
2023-03-20 19:15:49 +0100

    add h1

--- src/components/services/servicesDetail/TitleServiceDetail/index.jsx
+++ src/components/services/servicesDetail/TitleServiceDetail/index.jsx
@@ -3,8 +3,8 @@ import Container from "../../../shared/composers/container";
 const TitleServiceDetail = ({ title, title2 }) => {
   return (
     <Container className='text-[48px] md:text-[100px] mx-auto  pb-1  font-bold text-center leading-[50px] md:leading-[80px] tracking-[0.23px] text-transparent bg-clip-text bg-gradient-to-r via-orangeLight from-rougeLight to-saumon w-fit'>
-      {title}
-      <p>{title2}</p>
+      <h1>{title}</h1>
+      <h1>{title2}</h1>
     </Container>
   );
 };
